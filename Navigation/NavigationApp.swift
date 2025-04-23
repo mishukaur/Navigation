aa9dfcd7a46a33f05ec456f98b548bf912f3c2cb
Manjot Kaur <manjotk2003@gmail.com>
2025-04-23 19:58:59 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Manjot Kaur on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
